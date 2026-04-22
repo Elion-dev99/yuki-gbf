dcd3378df21495bf50a8415eac24e6ef227b12cf
Elion-dev99 <elion.dev08@gmail.com>
2026-04-22 17:07:53 +0900

    Add renderWeapons function to display weapon cards

---
+++ js/renderweapons.js
@@ -0,0 +1,24 @@
+function renderWeapons(list) {
+  const container = document.getElementById("weapon-list");
+  container.innerHTML = "";
+
+  list.forEach(w => {
+    const card = document.createElement("div");
+    card.className = "weapon-card";
+
+    card.innerHTML = `
+      <div class="weapon-name">${w.name}</div>
+      <div class="weapon-info">
+        <span>${w.element}</span> /
+        <span>${w.weapon_type}</span> /
+        <span>${w.rarity.toUpperCase()}</span>
+      </div>
+      <div class="weapon-stats">
+        HP ${w.stats.min_hp} → ${w.stats.max_hp}<br>
+        ATK ${w.stats.min_atk} → ${w.stats.max_atk}
+      </div>
+    `;
+
+    container.appendChild(card);
+  });
+}
