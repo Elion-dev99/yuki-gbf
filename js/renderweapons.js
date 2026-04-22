e798f078328abdb97aa9e5de428e400eebffbb65
Elion-dev99 <elion.dev08@gmail.com>
2026-04-22 18:38:59 +0900

    Refactor weapon card rendering to use destructured variables

--- js/renderweapons.js
+++ js/renderweapons.js
@@ -6,10 +6,19 @@ function renderweapons(list) {
     const card = document.createElement("div");
     card.className = "weapon-card";
 
+    const name = w.name;
+    const element = w.element;
+    const type = w.weapon_type;
+    const rarity = w.rarity;
+
+    // convert.js の JSON 構造に合わせる
+    const atk = w.stats?.max_atk ?? "";
+    const hp = w.stats?.max_hp ?? "";
+
     card.innerHTML = `
-      <div class="weapon-name">${w.name}</div>
-      <div class="weapon-info">${w.element} / ${w.weapon_type} / ${w.rarity}</div>
-      <div class="weapon-stats">ATK: ${w.atk} / HP: ${w.hp}</div>
+      <div class="weapon-name">${name}</div>
+      <div class="weapon-info">${element} / ${type} / ${rarity}</div>
+      <div class="weapon-stats">ATK: ${atk} / HP: ${hp}</div>
     `;
 
     container.appendChild(card);
