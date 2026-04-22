236eaeb664f5add1df4c7bbb59edb037fef39d0f
Elion-dev99 <elion.dev08@gmail.com>
2026-04-22 17:59:57 +0900

    Add async function to load weapons from JSON

---
+++ js/loadweapons.js
@@ -0,0 +1,13 @@
+async function loadweapons() {
+  // index.json を読み込む
+  const index = await fetch("data/weapons/index.json").then(r => r.json());
+  const weapons = [];
+
+  // index.json に書かれたパスを順番に読み込む
+  for (const path of index) {
+    const json = await fetch(`data/weapons/${path}`).then(r => r.json());
+    weapons.push(json);
+  }
+
+  return weapons;
+}
